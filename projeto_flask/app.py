3d7df6f8f3b544e6217c3c91d8bc1ffe410c1937
edvaniogomes <edvaniosenac@gmail.com>
2024-10-18 21:48:38 -0300

    Flask

---
+++ projeto_flask/app.py
@@ -0,0 +1,15 @@
+# A Importação do Flask e dos modulos necessários da Flash
+from flask import Flask, render_template, request
+
+# Criar uma Instância da aplicação Flask, o nom,e '__name__'
+# Define a rota principal da aplicação. Essa função e chamada
+# Quando acessanos a URI nada !!!
+return f'''
+<h1>Obrigado, {nome}</h1>
+<p>Recebemos sua mensagem:</p>
+<p><strong>E-mail:<strong> {email}</p>
+<p><strong>Messagem:<strong> {mensagem}</p>
+<p><strong>tema:<strong> {temal}</p>
+                           
+if__nane__== '__main--'
+app.run(debug=True)
